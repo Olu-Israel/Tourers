d91d773c96ac35f286ca6ef5e5abf25092d645fd
Ixrael <olutokijuwon12@gmail.com>
2024-10-04 09:58:39 +0100

    Modify routes

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Welcome to the Server Side!', app: 'Tourers' });
-});
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Welcome to the Server Side!', app: 'Tourers' });
+// });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint....');
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint....');
+// });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'Success',
+    data: {
+      tours: tours,
+    },
+  });
 });
 
 const port = 3000;
