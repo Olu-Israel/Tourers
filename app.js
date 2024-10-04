f475dfa413b331c2ed03fc0efee502dc5dc72721
Ixrael <olutokijuwon12@gmail.com>
2024-10-04 10:30:51 +0100

    modify the post route

--- app.js
+++ app.js
@@ -2,15 +2,8 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: 'Welcome to the Server Side!', app: 'Tourers' });
-// });
+app.use(express.json());
 
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint....');
-// });
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -21,6 +14,25 @@ app.get('/api/v1/tours', (req, res) => {
     result: tours.length,
     tours: tours,
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign(req.body, { id: newId });
+  tours.push(newTour);
+  fs.writeFileSync(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        message: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
