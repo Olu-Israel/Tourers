948efce0c2f6d5fc2fe966c9ea8c2e0002638573
Ixrael <olutokijuwon12@gmail.com>
2024-10-07 09:49:56 +0100

    Modify api

--- app.js
+++ app.js
@@ -17,22 +17,10 @@ app.get('/api/v1/tours', (req, res) => {
 });
 
 app.post('/api/v1/tours', (req, res) => {
-  // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign(req.body, { id: newId });
+  const newTour = Object.assign({ id: newId }, req.body);
+
   tours.push(newTour);
-  fs.writeFileSync(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        message: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
 });
 
 const port = 3000;
